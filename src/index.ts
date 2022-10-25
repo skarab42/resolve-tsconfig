a75004aee39bfbe1e78d528d78c8034132e0566d
skarab42 <skarab@bluewin.ch>
2022-10-25 16:51:20 +0200

    feat: add `findConfigFile`

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,82 @@
-export const api = { life: 42 };
+import fs from 'node:fs';
+import path from 'node:path';
+import ts from 'typescript';
+
+export function normalizePathSeparator(path: string): string {
+  return path.replace(/[/\\]+/g, '/');
+}
+
+function findFileUp(
+  directory: string,
+  stopDirectory: string | undefined,
+  callback: (directory: string) => string | undefined,
+): string | undefined {
+  const result = callback(directory);
+
+  if (result !== undefined) {
+    return result;
+  }
+
+  if (directory === stopDirectory) {
+    return undefined;
+  }
+
+  const parentPath = path.dirname(directory);
+
+  if (parentPath === directory) {
+    return undefined;
+  }
+
+  return findFileUp(parentPath, stopDirectory, callback);
+}
+
+type Options = {
+  startDirectory?: string | undefined;
+  stopDirectory?: string | undefined;
+  startDirectoryShouldExists?: boolean | undefined;
+};
+
+type NormalizedOptions = {
+  fileName: string;
+  startDirectory: string;
+  stopDirectory: string | undefined;
+  startDirectoryShouldExists: boolean | undefined;
+};
+
+function normalizeInput(filePath: string, options: Options): NormalizedOptions | never {
+  const absolutePath = normalizePathSeparator(path.resolve(options.startDirectory ?? process.cwd(), filePath));
+  const startDirectory = path.dirname(absolutePath);
+
+  if (options.startDirectory && path.isAbsolute(filePath)) {
+    const providedStartDirectory = normalizePathSeparator(path.resolve(options.startDirectory));
+
+    if (startDirectory !== providedStartDirectory) {
+      throw new Error(
+        `Do not specify the 'startDirectory' option if you give an absolute 'filePath' or they must be equal. Expected '${startDirectory}', received '${providedStartDirectory}'.`,
+      );
+    }
+  }
+
+  if (options.startDirectoryShouldExists && !fs.existsSync(startDirectory)) {
+    throw new Error(
+      `The start directory '${startDirectory}' does not exists but it should because you have set the 'startDirectoryShouldExists' option to true.`,
+    );
+  }
+
+  return {
+    fileName: path.basename(absolutePath),
+    startDirectory,
+    startDirectoryShouldExists: Boolean(options.startDirectoryShouldExists),
+    stopDirectory: options.stopDirectory ? normalizePathSeparator(path.resolve(options.stopDirectory)) : undefined,
+  };
+}
+
+export function findConfigFile(filePath = 'tsconfig.json', options: Options = {}): string | undefined {
+  const { fileName, startDirectory, stopDirectory } = normalizeInput(filePath, options);
+
+  return findFileUp(startDirectory, stopDirectory, (directory) => {
+    const filePath = `${directory}/${fileName}`;
+
+    return ts.sys.fileExists(filePath) ? filePath : undefined;
+  });
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,48 @@
+import path from 'node:path';
 import { expect, test } from 'vitest';
-import { api } from '../src/index.js';
+import { findConfigFile, normalizePathSeparator } from '../src/index.js';
 
-test('The Ultimate Question of Life', () => {
-  expect(api.life).toBe(42);
+const cwd = normalizePathSeparator(process.cwd());
+
+test(`relative path`, () => {
+  expect(findConfigFile()).toStrictEqual(`${cwd}/tsconfig.json`);
+  expect(findConfigFile(`tsconfig.json`)).toStrictEqual(`${cwd}/tsconfig.json`);
+  expect(findConfigFile(`package/tsconfig.json`)).toStrictEqual(`${cwd}/tsconfig.json`);
+  expect(findConfigFile(`./package/tsconfig.json`)).toStrictEqual(`${cwd}/tsconfig.json`);
+  expect(findConfigFile(`tsconfig.json`, { startDirectory: `package` })).toStrictEqual(`${cwd}/tsconfig.json`);
+  expect(findConfigFile(`tsconfig.json`, { startDirectory: `./package` })).toStrictEqual(`${cwd}/tsconfig.json`);
+  expect(findConfigFile(`./tsconfig.json`, { startDirectory: `package` })).toStrictEqual(`${cwd}/tsconfig.json`);
+  expect(findConfigFile(`./tsconfig.json`, { startDirectory: `./package` })).toStrictEqual(`${cwd}/tsconfig.json`);
+});
+
+test(`absolute path`, () => {
+  expect(findConfigFile(path.resolve(`tsconfig.json`))).toStrictEqual(`${cwd}/tsconfig.json`);
+  expect(findConfigFile(path.resolve(`tsconfig.json`), { startDirectory: `.` })).toStrictEqual(`${cwd}/tsconfig.json`);
+  expect(findConfigFile(path.resolve(`tsconfig.json`), { startDirectory: cwd })).toStrictEqual(`${cwd}/tsconfig.json`);
+  expect(findConfigFile(path.resolve(`tsconfig.json`), { startDirectory: process.cwd() })).toStrictEqual(
+    `${cwd}/tsconfig.json`,
+  );
+
+  expect(() => findConfigFile(path.resolve(`tsconfig.json`), { startDirectory: `./package` })).toThrow(
+    `Do not specify the 'startDirectory' option if you give an absolute 'filePath' or they must be equal. Expected '${cwd}', received '${cwd}/package'.`,
+  );
+});
+
+test(`stop directory`, () => {
+  expect(findConfigFile(`tsconfig.json`, { stopDirectory: `..` })).toStrictEqual(`${cwd}/tsconfig.json`);
+
+  expect(
+    findConfigFile(`tsconfig.json`, { startDirectory: `./package/client`, stopDirectory: `./package` }),
+  ).toBeUndefined();
+});
+
+test(`deep path`, () => {
+  expect(findConfigFile(`deep/path/that/does/not/exists/tsconfig.json`)).toStrictEqual(`${cwd}/tsconfig.json`);
+  expect(findConfigFile(`deep/path/that/does/not-exists.json`)).toBeUndefined();
+
+  expect(() =>
+    findConfigFile(`deep/path/that/does/not/exists/tsconfig.json`, { startDirectoryShouldExists: true }),
+  ).toThrow(
+    `The start directory '${cwd}/deep/path/that/does/not/exists' does not exists but it should because you have set the 'startDirectoryShouldExists' option to true.`,
+  );
 });
